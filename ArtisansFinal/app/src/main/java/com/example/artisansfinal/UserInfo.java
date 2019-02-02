4e75afbe274372b9e9e4417b6ae6ae79a992dcf4
Sabios-97 <sayan.biswas089@gmail.com>
2019-02-02 23:31:59 +0530

    added login and registration activity

---
+++ ArtisansFinal/app/src/main/java/com/example/artisansfinal/UserInfo.java
@@ -0,0 +1,13 @@
+package com.example.artisansfinal;
+
+public class UserInfo {
+    public String userName;
+    public String userPcode;
+    public String userPnumber;
+
+    public UserInfo(String userName, String userPcode, String userPnumber) {
+        this.userName = userName;
+        this.userPcode = userPcode;
+        this.userPnumber = userPnumber;
+    }
+}
