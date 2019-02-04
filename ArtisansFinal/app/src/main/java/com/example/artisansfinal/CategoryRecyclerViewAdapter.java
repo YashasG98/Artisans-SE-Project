6fd4a2f7d6ac2bd67c9334b8401c481fb26a398e
Rohit M P <rohitmp99@gmail.com>
2019-02-04 22:21:25 +0530

    Added progress bar for loading images

--- ArtisansFinal/app/src/main/java/com/example/artisansfinal/CategoryRecyclerViewAdapter.java
+++ ArtisansFinal/app/src/main/java/com/example/artisansfinal/CategoryRecyclerViewAdapter.java
@@ -8,6 +8,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -16,18 +17,28 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.PicassoProvider;
 
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
+
+import static com.bumptech.glide.Glide.with;
+import static com.squareup.picasso.Picasso.*;
 
 public class CategoryRecyclerViewAdapter extends RecyclerView.Adapter<CategoryRecyclerViewAdapter.ViewHolder>{
 
@@ -45,6 +56,7 @@ public class CategoryRecyclerViewAdapter extends RecyclerView.Adapter<CategoryRe
         TextView productPrice;
         TextView artisanName;
         RelativeLayout layout;
+        ProgressBar progressBar;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             productName = itemView.findViewById(R.id.category_layout_tv_product_name);
@@ -52,6 +64,7 @@ public class CategoryRecyclerViewAdapter extends RecyclerView.Adapter<CategoryRe
             productPrice = itemView.findViewById(R.id.category_layout_tv_product_price);
             artisanName = itemView.findViewById(R.id.category_layout_tv_artisan_name);
             layout = itemView.findViewById(R.id.category_layout_rl);
+            progressBar = itemView.findViewById(R.id.category_layout_pb_progress);
         }
     }
 
@@ -82,7 +95,23 @@ public class CategoryRecyclerViewAdapter extends RecyclerView.Adapter<CategoryRe
             storageReference.getBytes(ONE_MB).addOnSuccessListener(new OnSuccessListener<byte[]>() {
                 @Override
                 public void onSuccess(byte[] bytes) {
-                    Glide.with(context).load(bytes).into(viewHolder.image);
+                    Glide.with(context)
+                            .load(bytes)
+                            .listener(new RequestListener<Drawable>() {
+                                @Override
+                                public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                    viewHolder.progressBar.setVisibility(View.GONE);
+                                    return false;
+                                }
+
+                                @Override
+                                public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                    viewHolder.progressBar.setVisibility(View.GONE);
+                                    return false;
+                                }
+                            })
+                            .into(viewHolder.image);
+
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
