8775486f2095e8e0389fd6c7255c45f2bb5db070
Dhanasekhar <sekhardhana529@gmail.com>
2019-03-10 12:40:28 +0530

    added UI for artisan login and reviews

--- ArtisansFinal/app/src/main/java/com/example/artisansfinal/UserLoginFragment.java
+++ ArtisansFinal/app/src/main/java/com/example/artisansfinal/UserLoginFragment.java
@@ -60,7 +60,8 @@ public class UserLoginFragment extends Fragment {
         progressDialog = new ProgressDialog(getContext());
         FirebaseUser user = firebaseAuth.getCurrentUser();
         if (user != null) {
-            if(user.getPhoneNumber().length() == 0) {
+            String check = user.getEmail();
+            if(check != null && !check.isEmpty()) {
                 Log.d("HERE1",user.getEmail());
                 Intent intent1 = new Intent(new Intent(getContext(), UserHomePageActivity.class));
                 //Log.d("HERE1",Name.getText().toString().trim());
